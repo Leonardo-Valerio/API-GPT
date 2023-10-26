a755b0ec7bbf3df6f33267f5b344a983dc2e5f4a
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2023-10-25 22:23:44 -0300

    desafios realizados, código que simula envio de emails personalizados atráves da api do chat gpt funcionando e curso concluído

--- aula4/video1/avaliador_de_produtos.py
+++ aula4/video1/avaliador_de_produtos.py
@@ -2,7 +2,7 @@ import openai
 import os
 import dotenv
 import tiktoken
-
+import time
 def ler_arquivo(nome_arquivo):
     try:
         with open(nome_arquivo, 'r') as arquivo:
@@ -35,29 +35,44 @@ def analisar_produtos(nome_produto):
     """
     print(f'preparando analise {nome_produto}...')
     prompt_usuario = ler_arquivo(f"../dados/avaliacoes_{nome_produto}.txt")
+    tentativas = 0
+    tempo_exponencial = 5
+    while tentativas < 3:
+        tentativas += 1
+        try:
+            resposta = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": prompt_sistema
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt_usuario
+                    }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            adiciona_arquivo(f'../dados/analise_{nome_produto}', resposta.choices[0].message.content)
+            print(f'analise {nome_produto} realizada com sucesso')
+            return
+        except openai.error.AuthenticationError as e:
+            print(f'ERRO DE AUTENTICAÇÃO: {e}')
+        except openai.error.APIError as e:
+            print(f'ERRO DE API: {e}')
+            time.sleep(5)
+        except openai.error.RateLimitError as e:
+            print(f'ERRO LIMITE DE TAXA: {e}')
+            time.sleep(tempo_exponencial)
+            tempo_exponencial *= 2
 
-    resposta = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": prompt_sistema
-            },
-            {
-                "role": "user",
-                "content": prompt_usuario
-            }
-        ],
-        temperature=1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
 
-    adiciona_arquivo(f'../dados/analise_{nome_produto}', resposta.choices[0].message.content)
-    print(f'analise {nome_produto} realizada com sucesso')
-produtos = ['balança','boia_inflavel','DVD']
+produtos = ['balança','boia_inflavel','DVD','esteira','grill','jogo_de_copos','miniaturas_carros','mixer','tabuleiro_xadrez','tapete_yoga']
 
 for i in produtos:
     analisar_produtos(i)
